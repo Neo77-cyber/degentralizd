55b6dad2726e73da1fc5ea6c8a87a2a7b45650c3
sxample <bolaji_makinde@ymail.com>
2024-01-22 19:28:17 +0000

    navfix

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,19 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const topNav = document.getElementById("topNav");
-    const hamburger = document.getElementById("hamburger");
-    const menu = document.getElementById("menu");
-  
-    hamburger.addEventListener("click", function () {
-      console.log("clicked");
-      menu.classList.toggle("show");
-    });
-  
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 0) {
-        topNav.classList.add("scrolled");
-      } else {
-        topNav.classList.remove("scrolled");
-      }
-    });
-  });
-  
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
